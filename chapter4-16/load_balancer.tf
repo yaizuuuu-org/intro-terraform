c6dde55cfe0f86ab78851cd6e58b943d98c9704a
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-07-18 16:11:18 +0900

    18~20章 写経完

--- chapter4-16/load_balancer.tf
+++ chapter4-16/load_balancer.tf
@@ -3,7 +3,6 @@ resource "aws_lb" "example" {
   load_balancer_type         = "application"
   internal                   = false
   idle_timeout               = 60
-  enable_deletion_protection = true
 
   subnets = [
     aws_subnet.public_0.id,
