b2a3d7b6994aa30c3f46eadd6c73b0900e19bffa
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-07-19 14:33:01 +0900

    build test

--- chapter27/continuous/scripts/build.sh
+++ chapter27/continuous/scripts/build.sh
@@ -5,7 +5,7 @@ set -x
 pwd
 
 if [[ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/master' ]]; then
-    ${CODEBUILD_SRC_DIR}/chapter27/continuous/scripts/apply.sh
+    sh ${CODEBUILD_SRC_DIR}/chapter27/continuous/scripts/apply.sh
 else
-    ${CODEBUILD_SRC_DIR}/chapter27/continuous/scripts/plan.sh
+    sh ${CODEBUILD_SRC_DIR}/chapter27/continuous/scripts/plan.sh
 fi
