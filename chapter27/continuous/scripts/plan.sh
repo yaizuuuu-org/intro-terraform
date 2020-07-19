e19ae21b5055496b7e1a5530820034ad92f2ff6a
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-07-19 14:57:56 +0900

    build test

--- chapter27/continuous/scripts/plan.sh
+++ chapter27/continuous/scripts/plan.sh
@@ -6,7 +6,7 @@ pwd
 
 ls -al
 
-terraform init -imput=false -no-color
+terraform init -input=false -no-color
 terraform plan -input=false -no-color
 
 tfnotify --config ${CODEBUILD_SRC_DIR}/chapter27/continuous/tfnotify.yml plan --message "$(date)"
