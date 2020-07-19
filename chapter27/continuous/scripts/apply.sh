59702db7e61baaecc1b7044a8ccbc2839f9f5a37
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-07-19 14:36:46 +0900

    build test

--- chapter27/continuous/scripts/apply.sh
+++ chapter27/continuous/scripts/apply.sh
@@ -5,7 +5,7 @@ cd ${CODEBUILD_SRC_DIR}/chapter27/continuous
 pwd
 
 MESSAGE=$(git log ${CODEBUILD_SOURCE_VERSION} -1 --pretty=format:"%s")
-CODEBUILD_SOURCE_VERSION=$(echo ${MESSAGE} | cut -f4 -d'' | sed 's/#/pr\//')
+CODEBUILD_SOURCE_VERSION=$(echo ${MESSAGE} | cut -f4 -d' ' | sed 's/#/pr\//')
 
 terraform init -input=false -no-color
 terraform apply -input=false -no-color -auto-approve
